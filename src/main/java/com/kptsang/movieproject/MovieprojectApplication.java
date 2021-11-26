4cf57f5089b4a5b8f52379b2c3a50a64493f0eff
kapo <katsang15@gmail.com>
2021-11-26 13:16:09 +0000

    demo

--- src/main/java/com/kptsang/movieproject/MovieprojectApplication.java
+++ src/main/java/com/kptsang/movieproject/MovieprojectApplication.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @CrossOrigin(origins = "http://localhost:3000")
 public class MovieprojectApplication {
 
-
+//
     @Autowired
     private ActorRepository actorRepository;
     @Autowired
